cceb4efc9a6ce4db182c1d4c02cf2706f2715c22
JP Ferreira <jpferreira@MA301.broadband>
2021-05-04 16:02:58 +0100

    Complete Step 5: top_up card method to increase balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,12 +5,19 @@ describe Oystercard do
     # In order to use public transport
     # As a customer
     # I want money on my card
-    describe '#new_card' do
-        it 'returns a new oystercard with a balance of 0' do
-            oystercard = Oystercard.new
-            expect(oystercard.balance).to eq(0)
-        end
+    it 'returns a new oystercard with a balance of 0' do
+        oystercard = Oystercard.new
+        expect(oystercard.balance).to eq(0)
     end
 
+    # In order to keep using public transport
+    # As a customer
+    # I want to add money to my card
+    describe '#top_up' do
+        it { is_expected.to respond_to(:top_up).with(1).argument }
+        it 'tops up an oystercard with the value provided' do
+            expect { subject.top_up(10) }.to change { subject.balance }.by 10
+        end
+    end
 
 end
