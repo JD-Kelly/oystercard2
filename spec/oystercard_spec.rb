c9c64bbd434c93daffccf15d97668516b34121ac
JP Ferreira <jpferreira@MA301.broadband>
2021-05-04 15:39:15 +0100

    Complete Step 4: Add a test for balance, refactor balance method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,16 @@
+require 'oystercard'
+
 describe Oystercard do
     
+    # In order to use public transport
+    # As a customer
+    # I want money on my card
+    describe '#new_card' do
+        it 'returns a new oystercard with a balance of 0' do
+            oystercard = Oystercard.new
+            expect(oystercard.balance).to eq(0)
+        end
+    end
+
+
 end
