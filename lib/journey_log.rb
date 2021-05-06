9b3f548d8d4789e0b96b2a97dc6b0a0cb1bfa1fb
“Jonny <jonny.kelly192@gmail.com>
2021-05-06 17:49:08 +0100

    adds journey log class

---
+++ lib/journey_log.rb
@@ -0,0 +1,11 @@
+class JourneyLog
+    def initialize(journey_class: )
+        @journey_class = journey_class
+        @journeys = []
+    end
+
+    def start(station)
+        @journey_class.new(entry_station: station)
+    end
+
+end
