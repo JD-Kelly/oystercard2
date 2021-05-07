9a09ab093a3a9ac3857c5df3d8348660f6f3ed1c
BecaLParker <becaparker@gmail.com>
2021-05-07 14:38:22 +0100

    records a journey

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class JourneyLog
+  attr_reader :journeys
+
   def initialize(journey_class:)
     @journey_class = journey_class
     @journeys = []
   end
 
   def start(station)
-    @journey_class.new(entry_station: station)
+    @journeys << @journey_class.new(entry_station: station)
   end
 end
